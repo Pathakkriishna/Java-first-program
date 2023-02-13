1192505bbc425dab25d2f6117f73dd99bbee7573
Pathakkriishna <kriishna.pathak@gmail.com>
2023-02-13 10:32:47 +0545

    initial commit

---
+++ Head.java
@@ -0,0 +1,8 @@
+public class Head {
+    public static void main(String[] args){
+        System.out.println("Name : Krishna Pathak");
+        System.out.println("Class : BIT 6Th Semester");
+        System.out.println("Rollno : 4703");
+        System.out.println("Subject : Advance OOP");
+    }
+}
